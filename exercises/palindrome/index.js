9634c00057a686e8c27ab6ab7da1662e5a30edc1
beckychan92 <beckychan92@gmail.com>
2019-02-24 02:57:02 -0800

    palidrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,28 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome1(str) {
+    var reversed = str.split('').reverse().join('')
+    return str === reversed
+
+}
+
+function palindrome(str) {
+    var reversed = str.split('').reduce((word, char) => char + word , '')
+    return reversed === str
+}
+
+
+
+
+
+console.log(palindrome('aba') === true)
+console.log(palindrome(' aba') === false)
+console.log(palindrome('1000000001') === true)
+console.log(palindrome('aba ') === false)
+console.log(palindrome('greetings') === false)
+console.log(palindrome('Fish hsif') === false)
+console.log(palindrome('pennep') === true)
+
 
 module.exports = palindrome;
