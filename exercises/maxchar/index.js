63e29338f6b16fd128a998ffa5b46b688609f2a2
beckychan92 <beckychan92@gmail.com>
2019-02-24 18:48:30 -0800

    toy problems

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,43 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar1(str) {
+    var hash = {};
+    str.split('').forEach((char) => hash[char] ? hash[char]++ : hash[char] = 1)
+    var char, count = 0;
+    for(var key in hash) {
+        if(hash[key] > count) {
+            count = hash[key];
+            char = key;
+        }
+    }
+    return char
+}
 
+function maxChar(str) {
+    var hash = {};
+    var char, count = 0;
+    for(var key of str) {
+        if(hash[key]) {
+            hash[key]++;
+        } else {
+            hash[key] = 1
+        }
+    }
+    for(var key in hash) {
+        if(hash[key] > count) {
+            count = hash[key];
+            char = key
+        }
+    }
+    return char;
+}
+
+
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+console.log(maxChar("abcccccccd"))
+console.log(maxChar("apple 1231111"))
+console.log(maxChar(""))
 module.exports = maxChar;
